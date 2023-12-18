494c3f621108021d9a4243038eb17a9fedb5bf1b
Mohammad Ashif <Mohammad Ashif>
2023-12-18 12:28:54 +0600

    popular menu section edit done

--- src/Pages/Home/FeaturedFood/FeaturedFood.jsx
+++ src/Pages/Home/FeaturedFood/FeaturedFood.jsx
@@ -4,18 +4,23 @@ import MenuItem from "../../../components/MenuItem/MenuItem";
 const FeaturedFood = () => {
     const [menu, setMenu] = useState([]);
 
-    const items = menu.filter(item => item.category === 'popular');
-   
+    const items = menu.filter((item) => item.category === "popular");
+
     useEffect(() => {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => setMenu(data))
-    },[])
+        fetch("menu.json")
+            .then((res) => res.json())
+            .then((data) => setMenu(data));
+    }, []);
     return (
-        <div className="w-3/4 mx-auto grid md:grid-cols-2 gap-5 my-10">
-            {
-                items.map(item => <MenuItem key={item._key} item={item}></MenuItem>)
-            }
+        <div className="my-10">
+            <div className="w-3/4 mx-auto grid md:grid-cols-2 gap-5 my-10">
+                {items.map((item) => (
+                    <MenuItem key={item._key} item={item}></MenuItem>
+                ))}
+            </div>
+            <div className="text-center">
+                <button className="btn btn-outline border-b-4">View Full Menu</button>
+            </div>
         </div>
     );
 };
--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,29 +1,20 @@
-const MenuItem = ({item}) => {
-    const {image, price, name, recipe} = item;
+const MenuItem = ({ item }) => {
+    const { image, price, name, recipe } = item;
     return (
-        // <div className="card  bg-base-100 shadow-xl">
-        //     <figure>
-        //         <img
-        //             src={image}
-        //             alt="Shoes"
-        //         />
-        //     </figure>
-        //     <p>{price}</p>
-        //     <div className="card-body">
-        //         <h2 className="card-title">{name}</h2>
-        //         <p>{recipe}</p>
-        //         <div className="card-actions justify-end">
-        //             <button className="btn btn-primary">Buy Now</button>
-        //         </div>
-        //     </div>
-        // </div>
-        <div className="flex gap-8 items-center">
-            <img className="w-[100px] h-[80px] rounded-r-full rounded-bl-full" src={image} alt="" />
-            <div>
-                <h2 className="text-[22px] my-2">{name}-----------</h2>
-                <p>{recipe}</p>
+        <div>
+            <div className="flex gap-8 items-center">
+                <img
+                    className="w-[100px] h-[80px] rounded-r-full rounded-bl-full"
+                    src={image}
+                    alt=""
+                />
+                <div>
+                    <h2 className="text-[22px] my-2">{name}-----------</h2>
+                    <p>{recipe}</p>
+                </div>
+                <p className="text-[20px] text-[#BB8506]">${price}</p>
             </div>
-            <p className="text-[20px] text-[#BB8506]">${price}</p>
+            
         </div>
     );
 };
