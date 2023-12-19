c13cd78151bef4c6ff8dd9a9421d340d168afc52
Mohammad Ashif <Mohammad Ashif>
2023-12-19 20:15:49 +0600

    menu section edit done

--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,14 +1,21 @@
 import Cover from "../../../components/Cover/Cover";
 import MenuItem from "../../../components/MenuItem/MenuItem";
 
-const MenuCategory = ({items, coverImage, coverTitle}) => {
+const MenuCategory = ({ items, coverImage, coverTitle }) => {
     return (
-        <div className="w-3/4 mx-auto">
-            {coverTitle && <Cover image={coverImage} title={coverTitle}></Cover>}
-            <div className=" mx-auto grid md:grid-cols-2 gap-5 my-10">
+        <div className="my-10">
+            {coverTitle && (
+                <Cover image={coverImage} title={coverTitle}></Cover>
+            )}
+            <div className="w-3/4 mx-auto grid md:grid-cols-2 gap-5 my-10">
                 {items.map((item) => (
                     <MenuItem key={item._key} item={item}></MenuItem>
                 ))}
+            </div>
+            <div className="text-center">
+                <button className="btn btn-outline border-b-4">
+                    ORDER YOUR FAVORITE FOOD
+                </button>
             </div>
         </div>
     );
